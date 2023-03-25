dc7bc629c7f59c11384d9d4da8d502b4deb02f5a
Amponsem <amponsem@hotmail.com>
2023-03-25 13:27:19 -0400

    assignment 1

---
+++ diagnostic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IWhite='\033[1;97m'
+IGreen='\033[1;92m'
+Color_Off='\033[0m'
+
+echo -e "${IWhite}diagnostic starting...${Color_Off}"
+read -n 1 -r -s -p "Press any key to continue"
+echo ""
+touch diagnostic.log
+pwd >> diagnostic.log
+mysql --version >> diagnostic.log
+python3 --version >> diagnostic.log
+history >> diagnostic.log
+sleep 1
+echo -e "${IGreen}diagnostic completed!!!${Color_Off}"
+sleep 1
